03256a34b8497020b4c15affd276d5a1b581cc52
Steph <styu314@gmail.com>
2012-05-01 22:56:30 -0400

    created packing page

--- PackIt/src/edu/mit/packit/PackActivity.java
+++ PackIt/src/edu/mit/packit/PackActivity.java
@@ -1,7 +1,28 @@
 package edu.mit.packit;
 
+import edu.mit.packit.temp.PackTempActivity;
 import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
 
 public class PackActivity extends TabActivity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.packlayout);
+    	
+//        TabHost tabHost = getTabHost();  // The activity TabHost
+//        
+//        addTab(tabHost, PackTempActivity.SHIRTS);
+//        addTab(tabHost, PackTempActivity.JACKETS);
+//        addTab(tabHost, PackTempActivity.PANTS);
+//        addTab(tabHost, PackTempActivity.FORMAL_WEAR);
+//        addTab(tabHost, PackTempActivity.WINTER_GEAR);
+//        addTab(tabHost, PackTempActivity.UNDERWEAR);
+//        addTab(tabHost, PackTempActivity.MISC);
+//        
+//        tabHost.setCurrentTab(0);
+        
+    }
 }
