a76958f289a64169470a187390a97724cc9f7ead
Stephanie Yu <styu314@gmail.com>
2012-05-06 12:27:13 -0400

    made main menu scrollable

--- PackIt/src/edu/mit/packit/MainMenuActivity.java
+++ PackIt/src/edu/mit/packit/MainMenuActivity.java
@@ -5,16 +5,22 @@ import java.util.List;
 import edu.mit.packit.db.TripDetails;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainMenuActivity extends ListActivity {
+public class MainMenuActivity extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +28,27 @@ public class MainMenuActivity extends ListActivity {
         
         PackItActivity.datasource.open();
         List<String> trips = PackItActivity.datasource.getAllTripNames();
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.trip_item, trips));
-        
-        ListView lv = getListView();
-        lv.setTextFilterEnabled(true);
+        RelativeLayout trip_list = (RelativeLayout) findViewById(R.id.trip_list);
+		LayoutInflater vi = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		int cur_id = 0;
+        for (String trip_name : trips) {
+        	LinearLayout cur_trip = (LinearLayout) vi.inflate(R.layout.trip_item, null);
+        	TextView trip_text = (TextView) cur_trip.findViewById(R.id.trip_name);
+        	trip_text.setText(trip_name);
+        	cur_trip.setId(cur_id);
+        	if (cur_id != 0) {
+        		RelativeLayout.LayoutParams q = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                q.addRule(RelativeLayout.BELOW, cur_id - 1);
+        		cur_trip.setLayoutParams(q);
+        	}
+        	cur_id++;
+        	trip_list.addView(cur_trip);
+        }
+//        setListAdapter(new ArrayAdapter<String>(this, R.layout.trip_item, trips));
+//        
+//        ListView lv = getListView();
+//        lv.setTextFilterEnabled(true);
         
         Button continue_button = (Button) findViewById(R.id.continue_button);
         ImageView settings_button = (ImageView) findViewById(R.id.settings_button);
@@ -37,7 +60,7 @@ public class MainMenuActivity extends ListActivity {
         continue_button.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-				EditText trip_field = (EditText) ((Activity) v.getContext()).findViewById(R.id.trip_name);
+				EditText trip_field = (EditText) ((Activity) v.getContext()).findViewById(R.id.trip_name_field);
 				String trip_name = trip_field.getText().toString();
 				if (trip_name != null && trip_name.length() > 0) {
 					Intent intent = new Intent(v.getContext(),
